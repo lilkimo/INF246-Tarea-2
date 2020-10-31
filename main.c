c28e8285cdeafc467a47fb3a92a337dcc9542fd8
Daxo <65884095+Daxo-s@users.noreply.github.com>
2020-10-31 16:22:18 -0300

    queue implementation

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "queue.c"
 
 #define CANTIDADJUGADORES 4
 #define CANTIDADCASILLAS 29
 
 typedef int jugador; // 0: Jugador, {1, 2, 3}: Bot.
-typedef int casilla; // 0: Blanco, 1: ?, 2: ??, 3: Inicio, 4: Final.
+typedef int casilla; // 0: Blanco, 1: ?, 2: ??.
 
-casilla tablero[/*CANTIDADCASILLAS*/] = {3, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 2, 0, 0, 0, 0, 1, 2, 1, 2, 1, 2, 1, 4};
-int posiciones[/*CANTIDADJUGADORES*/] = {0, 0, 0, 0}; // Posiciones de los Jugadores.
-int sentido = 0; // 0: Normal, 1: Inverso.
-
-//Cola turnos.
+casilla tablero[] = {0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 2, 0, 0, 0, 0, 1, 2, 1, 2, 1, 2, 1, 0};
+int posiciones[] = {0, 0, 0, 0}; // Posiciones de los Jugadores.
+int sentido = 0; // 0: Inverso, 1: Normal.
+queue *colaTurnos/* = newqueue()*/;
 
 // Devuelve el tipo de casilla en la que "cayó" la ficha.
 casilla mover(const jugador jugador, const casilla *tablero, const int cantidadCasillas) {
@@ -25,6 +25,11 @@ casilla mover(const jugador jugador, const casilla *tablero, const int cantidadC
     return tablero[destino];
 }
 
+int tirarDado() {
+    return (rand()%6) + 1;
+}
+
 int main() {
+    srand(2);
     return 0;
 }
