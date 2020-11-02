e7d177986a4385b4455f2c698e664d1107f57b37
Crastiv <camilo.gonzalezn@sansano.usm.cl>
2020-11-02 00:35:39 -0300

    added comments

--- comunicacion.c
+++ comunicacion.c
@@ -14,11 +14,26 @@ int pipeP_J3[2];
 Pipe *vectorPipesJx_P[] = {pipeJ0_P, pipeJ1_P, pipeJ2_P, pipeJ3_P};
 Pipe *vectorPipesP_Jx[] = {pipeP_J0, pipeP_J1, pipeP_J2, pipeP_J3};
 
+/*
+Nombre:  crearMemoriaCompartida
+Recibe:
+         size: Tamaño de la memoria compartida.
+Retorna: void.
+Hace:    crea una region en la memoria compartida de tamaño size.
+*/
+
 void* crearMemoriaCompartida(size_t size){
     int protection = PROT_READ | PROT_WRITE;
     int visibilty = MAP_SHARED | MAP_ANONYMOUS;
     return mmap(NULL, size, protection, visibilty, -1, 0);
 }
+
+/*
+Nombre:  crearPipes
+Recibe: nada
+Retorna: nada.
+Hace: Crea las pipes para que se comuniquen los procesos, en total son 8, 2 entre cada proceso padre-hijo.
+*/
 
 void crearPipes(){
     for (int i = 0; i < 4; i++){
@@ -27,15 +42,15 @@ void crearPipes(){
     for (int i = 0; i < 4; i++){
         pipe(vectorPipesP_Jx[i]);
     }
-    /*pipe(pipeJ0_P);
-    pipe(pipeJ1_P);
-    pipe(pipeJ2_P);
-    pipe(pipeJ3_P);
-    pipe(pipeP_J0);
-    pipe(pipeP_J1);
-    pipe(pipeP_J2);
-    pipe(pipeP_J3);*/
 }
+
+/*
+Nombre:  manejoPipes
+Recibe:
+        id_jugador: ID del jugador que llama a la funcion.
+Retorna: void.
+Hace: Cierra el fd[READ] o fd[WRITE], dependiendo si el proceso los ocupa o no (ya que las pipes son unidireccionales).
+*/
 
 void manejoPipes(int id_jugador){
     if (id_jugador == -1){
@@ -43,14 +58,6 @@ void manejoPipes(int id_jugador){
             close(vectorPipesJx_P[i][WRITE]);
             close(vectorPipesP_Jx[i][READ]);
         }
-        /*close(pipeJ0_P[WRITE]);
-        close(pipeJ1_P[WRITE]);
-        close(pipeJ2_P[WRITE]);
-        close(pipeJ3_P[WRITE]);
-        close(pipeP_J0[READ]);
-        close(pipeP_J1[READ]);
-        close(pipeP_J2[READ]);
-        close(pipeP_J3[READ]);*/
     }
     else if(id_jugador == 0){
         close(pipeJ0_P[READ]);
@@ -69,6 +76,13 @@ void manejoPipes(int id_jugador){
         close(pipeP_J3[WRITE]);
     }
 }
+
+/*
+Nombre:  cerrarPipes
+Recibe:
+         idJugador: ID del jugador que llama la funcion.
+Hace: cierra las pipes restantes, para que no queden en memoria.
+*/
 
 void cerrarPipes(int idJugador){
     if (idJugador == -1){
@@ -95,11 +109,18 @@ void cerrarPipes(int idJugador){
     }
 }
 
+/*
+Nombre:  
+Recibe:
+        *id: Puntero que indica la id con la que se creara el hijo.
+Retorna: void
+Hace: Crea los 4 hijos (jugadores), cada uno con una id distinta.
+*/
+
 void crearHijos(int *id){
     for(int i = 0; i < 4; i++){
         if (fork() == 0){
             *id = i;
-            //printf("hijo %d creado\n",*id);
             break;
         }
     }
