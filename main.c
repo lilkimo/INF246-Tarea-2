e1295ef2ff137a66b73b487093ea252591500e89
Crastiv <camilo.gonzalezn@sansano.usm.cl>
2020-11-02 00:13:01 -0300

    change main

--- main.c
+++ main.c
@@ -16,94 +16,20 @@ int tirarDado() {
     return (rand()%6) + 1;
 }
 
-int ejecutarTurno(int jugadorActual, casilla *ptrTablero, int *ptrPosiciones, int *ptrSentido, queue *colaTurnos) {
-    int flag;
-    int mensaje;
-    int dado;
-    char decision[2];
-    casilla casilla;
-    //write(pipeJ0_P[WRITE], 2, 1);*
-    mensaje = 2;
-    write(vectorPipesJx_P[jugadorActual][WRITE], &mensaje, 1);
-    dado = tirarDado();
-    printf("Salio un %d\n", dado);
-    casilla = mover(jugadorActual, dado, ptrTablero, ptrPosiciones);
-    if (*ptrSentido == 1) {
-        if (ptrPosiciones[jugadorActual] == CANTIDADCASILLAS)
-            flag = 0;
-        else {
-            if (casilla == 0) {
-                //write(pipeJ0_P[WRITE], 1, 0);
-                mensaje = 1;
-                write(vectorPipesJx_P[jugadorActual][WRITE], &mensaje, 0);
-            }
-            else {
-                if (jugadorActual == 0) {
-                    printf("Desea ejecutar el efecto? Escriba 'Si' o 'No' \n");
-                    scanf("%s", decision);
-                    if (strcmp(decision,"Si") == 0)
-                        ejecutarEfecto(casilla, jugadorActual, ptrTablero, ptrPosiciones, ptrSentido, colaTurnos);
-                    else{
-                        mensaje = 1;
-                        write(vectorPipesJx_P[jugadorActual][WRITE], &mensaje, 1);
-                    }
-                }
-                else {
-                    mensaje = 1;
-                    ejecutarEfecto(casilla, jugadorActual, ptrTablero, ptrPosiciones, ptrSentido, colaTurnos);
-                    write(vectorPipesJx_P[jugadorActual][WRITE], &mensaje, 1);
-                }
-            }
-        }
-    }
-    else {
-        if (ptrPosiciones[jugadorActual] == 0)
-            flag = 0;
-        else {
-            if (casilla == 0){
-                //write(pipeJ0_P[WRITE], 1, 0);
-                mensaje = 1;
-                write(vectorPipesJx_P[jugadorActual][WRITE], &mensaje, 0);
-            }
-            else {
-                printf("Desea ejecutar el efecto? Escriba 'Si' o 'No'. \n");
-                scanf("%s", decision);
-                if (strcmp(decision,"Si") == 0)
-                    ejecutarEfecto(casilla, jugadorActual, ptrTablero, ptrPosiciones, ptrSentido, colaTurnos);
-                else {
-                    mensaje = 1;
-                    write(vectorPipesJx_P[jugadorActual][WRITE], &mensaje, 1);
-                }
-            }
-        }
-    }
-    return flag;
-}
-
-typedef int mensaje;
 casilla tablero[] = {0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 2, 0, 0, 0, 0, 1, 2, 1, 2, 1, 2, 1, 0};
 int posiciones[] = {0, 0, 0, 0}; // Posiciones de los Jugadores.
 int sentido = 1; // -1: Inverso, 1: Normal.
 
 int main() {
 
-    mensaje mensajeP_J0 = -1;
-    mensaje mensajeP_J1 = -1;
-    mensaje mensajeP_J2 = -1;
-    mensaje mensajeP_J3 = -1;
-    mensaje mensajeJ0_P = -1;
-    mensaje mensajeJ1_P = -1;
-    mensaje mensajeJ2_P = -1;
-    mensaje mensajeJ3_P = -1;
+    mensaje vectorMensajesP_Jx[] = {0, 0, 0, 0};
+    mensaje vectorMensajesJx_P[] = {0, 0, 0, 0};
 
-    mensaje vectorMensajesP_Jx[] = {mensajeP_J0, mensajeP_J1, mensajeP_J2, mensajeP_J3};
-    mensaje vectorMensajesJx_P[] = {mensajeJ0_P, mensajeJ1_P, mensajeJ2_P, mensajeJ3_P};
-
-    int idJugador = -1;
+    int idProceso = -1;
     int jugadorActual;
     int posJugador;
     int flag = 1;
-    queue *colaTurnos = newqueue(4);
+    queue *colaTurnos = newqueue(CANTIDADJUGADORES);
 
     casilla *ptrTablero = crearMemoriaCompartida(sizeof(tablero));
     int *ptrPosiciones = crearMemoriaCompartida(sizeof(posiciones));
@@ -115,191 +41,95 @@ int main() {
     *ptrSentido = sentido;
     *ptrFlag = flag;
 
-    printf("Ingrese el lugar en el que quiere ingresar: ");
+    printf("Ingrese el lugar en el que quiere ingresar: \n");
     scanf("%d", &posJugador);
-    switch (posJugador)
-    {
-    case 1:
-        enqueue(colaTurnos, 0);
-        enqueue(colaTurnos, 1);
-        enqueue(colaTurnos, 2);
-        enqueue(colaTurnos, 3);
-        break;
-    case 2:
-        enqueue(colaTurnos, 1);
-        enqueue(colaTurnos, 0);
-        enqueue(colaTurnos, 2);
-        enqueue(colaTurnos, 3);
-        break;
-    case 3:
-        enqueue(colaTurnos, 1);
-        enqueue(colaTurnos, 2);
-        enqueue(colaTurnos, 0);
-        enqueue(colaTurnos, 3);
-        break;
-    case 4:
-        enqueue(colaTurnos, 1);
-        enqueue(colaTurnos, 2);
-        enqueue(colaTurnos, 3);
-        enqueue(colaTurnos, 0);
-        break;
-    
-    default:
-        printf("error, solo puede ser < 4");
-        exit(0);
-        break;
+    posJugador = posJugador - 1; 
+    for (int i = 0, j = 1; i < CANTIDADJUGADORES; i++) {
+        if (i == posJugador)
+            enqueue(colaTurnos, 0);
+        else
+            enqueue(colaTurnos, j++);
     }
+    
+    printqueue(colaTurnos);
 
     crearPipes();
-    crearHijos(&idJugador);
-    manejoPipes(idJugador);
+    crearHijos(&idProceso);
+    manejoPipes(idProceso);
 
-    if (idJugador == -1){
-        printf("La cola de turnos: ");
-        printqueue(colaTurnos);
-        jugadorActual = dequeue(colaTurnos);
-        enqueue(colaTurnos, jugadorActual);
-        if (jugadorActual == 0)
-            printf("Es tu turno\n");
-        else {
-            printf("Es el turno del bot %d\n", jugadorActual);
+    if (idProceso == -1){
+        for (int i = 0; i < CANTIDADJUGADORES; i++){
+            write(vectorPipesP_Jx[i][WRITE], &vectorMensajesP_Jx[i], 1);
+        }
+    }
+    else {
+        write(vectorPipesJx_P[idProceso][WRITE], &vectorMensajesJx_P[idProceso], 1);
+    }
+    //printf("[%d, %d, %d, %d]\n",vectorMensajesP_Jx[0],vectorMensajesP_Jx[1],vectorMensajesP_Jx[2],vectorMensajesP_Jx[3]);
+    //printf("[%d, %d, %d, %d]\n",vectorMensajesJx_P[0],vectorMensajesJx_P[1],vectorMensajesJx_P[2],vectorMensajesJx_P[3]);
+    
+    while(*ptrFlag){
+        if (idProceso == -1) {
+            jugadorActual = dequeue(colaTurnos);
+            enqueue(colaTurnos, jugadorActual);
+
+            int turnoEnEjecucion = 1;
+
+            vectorMensajesP_Jx[jugadorActual] = 1;
+            write(vectorPipesP_Jx[jugadorActual][WRITE], &vectorMensajesP_Jx[jugadorActual], 1);
+            //printf("VM1:[%d, %d, %d, %d]\n",vectorMensajesP_Jx[0],vectorMensajesP_Jx[1],vectorMensajesP_Jx[2],vectorMensajesP_Jx[3]); //borrar
+            while (turnoEnEjecucion){
+                read(vectorPipesJx_P[jugadorActual][READ], &vectorMensajesJx_P[jugadorActual], 1);
+                if (vectorMensajesJx_P[jugadorActual] == 1) {
+                    while (turnoEnEjecucion){
+                        read(vectorPipesJx_P[jugadorActual][READ], &vectorMensajesJx_P[jugadorActual], 1);
+                        turnoEnEjecucion = 0;
+                    }
+                    vectorMensajesP_Jx[jugadorActual] = 0;
+                    write(vectorPipesP_Jx[jugadorActual][WRITE], &vectorMensajesP_Jx, 1);
+                }
+            }
+            int final = CANTIDADCASILLAS - 1;
+            if (*ptrSentido == -1){
+                int final = 0;
+            }
+
+            for (int i = 0; i < CANTIDADJUGADORES; i++){
+                if (ptrPosiciones[i] == final){
+                    printf("El ganador del juego es %d\n", i);
+                    *ptrFlag = 0;
+                    break;
+                }
+            }
+
+        }
+        else{
+            int esperandoTurno = 1;
+            while(esperandoTurno){
+                read(vectorPipesP_Jx[idProceso][READ], &vectorMensajesP_Jx[idProceso], 1);
+                if (vectorMensajesP_Jx[idProceso] == 1){
+                    esperandoTurno = 0;
+                }
+            }
+
+            vectorMensajesJx_P[idProceso] = 1;
+            write(vectorPipesJx_P[idProceso][WRITE], &vectorMensajesJx_P[idProceso], 1);
+
+
+            casilla casilla = mover(idProceso, tirarDado()*(*ptrSentido), ptrTablero, ptrPosiciones);
+            if (casilla){
+                ejecutarEfecto(casilla, idProceso, ptrTablero, ptrPosiciones, ptrSentido, colaTurnos);
+            }
+            vectorMensajesJx_P[idProceso] = 0;
+            write(vectorPipesJx_P[idProceso][WRITE], &vectorMensajesJx_P[idProceso], 1);
         }
     }
 
-    while (*ptrFlag) {
-        if (idJugador == -1) {
-            for (int i = 0; i < 4; i++) {
-                read(vectorPipesJx_P[i][READ], &vectorMensajesJx_P[i], 1);
-            }
-            /*read(pipeJ0_P[READ], &mensajeJ0_P, 1);
-            read(pipeJ0_P[READ], &mensajeJ1_P, 1);
-            read(pipeJ0_P[READ], &mensajeJ2_P, 1);
-            read(pipeJ0_P[READ], &mensajeJ3_P, 1);*/
-            if (mensajeJ0_P == 1 || mensajeJ1_P == 1 || mensajeJ2_P == 1 || mensajeJ3_P == 1) {
-                jugadorActual = dequeue(colaTurnos);
-                enqueue(colaTurnos, jugadorActual);
-                printf("Es el turno del jugador %d\n", jugadorActual+1);
-            }
-            if (jugadorActual == 0) {
-                for (int i = 0; i < 4; i++){
-                    if (i == jugadorActual) {
-                        vectorMensajesP_Jx[i] = 1;
-                        write(vectorPipesP_Jx[i][WRITE], &vectorMensajesP_Jx[i], 1);
-                    }
-                    else {
-                        vectorMensajesP_Jx[i] = 0;
-                        write(vectorPipesP_Jx[i][WRITE], &vectorMensajesP_Jx[i], 1);
-                    }
-                }
-                /*write(pipeP_J0[WRITE], 1, 1);
-                write(pipeP_J1[WRITE], 0, 1);
-                write(pipeP_J2[WRITE], 0, 1);
-                write(pipeP_J3[WRITE], 0, 1);*/
-            }
-            else if (jugadorActual == 1) {
-                for (int i = 0; i < 4; i++){
-                    if (i == jugadorActual) {
-                        vectorMensajesP_Jx[i] = 1;
-                        write(vectorPipesP_Jx[i][WRITE], &vectorMensajesP_Jx[i], 1);
-                    }
-                    else {
-                        vectorMensajesP_Jx[i] = 0;
-                        write(vectorPipesP_Jx[i][WRITE], &vectorMensajesP_Jx[i], 1);
-                    }
-                }
-                /*write(pipeP_J0[WRITE],0 , 1);
-                write(pipeP_J1[WRITE],1 , 1);
-                write(pipeP_J2[WRITE],0 , 1);
-                write(pipeP_J3[WRITE],0 , 1);*/
-            }
-            else if (jugadorActual == 2) {
-                for (int i = 0; i < 4; i++){
-                    if (i == jugadorActual) {
-                        vectorMensajesP_Jx[i] = 1;
-                        write(vectorPipesP_Jx[i][WRITE], &vectorMensajesP_Jx[i], 1);
-                    }
-                    else {
-                        vectorMensajesP_Jx[i] = 0;
-                        write(vectorPipesP_Jx[i][WRITE], &vectorMensajesP_Jx[i], 1);
-                    }
-                }
-                /*write(pipeP_J0[WRITE],0 , 1);
-                write(pipeP_J1[WRITE],0 , 1);
-                write(pipeP_J2[WRITE],1 , 1);
-                write(pipeP_J3[WRITE],0 , 1);*/
-            }
-            else {
-                for (int i = 0; i < 4; i++){
-                    if (i == jugadorActual) {
-                        vectorMensajesP_Jx[i] = 1;
-                        write(vectorPipesP_Jx[i][WRITE], &vectorMensajesP_Jx[i], 1);
-                    }
-                    else {
-                        vectorMensajesP_Jx[i] = 0;
-                        write(vectorPipesP_Jx[i][WRITE], &vectorMensajesP_Jx[i], 1);
-                    }
-                }
-                /*write(pipeP_J0[WRITE],0 , 1);
-                write(pipeP_J1[WRITE],0 , 1);
-                write(pipeP_J2[WRITE],0 , 1);
-                write(pipeP_J3[WRITE],1 , 1);*/
-            }
-        }
-        else if (idJugador == 0) {
-            read(pipeP_J0[READ], &mensajeP_J0, 1);
-            if (mensajeJ0_P == 1) {
-                *ptrFlag = ejecutarTurno(jugadorActual, ptrTablero, ptrPosiciones, ptrSentido, colaTurnos);
-            }
-            else {
-                vectorMensajesJx_P[jugadorActual] = 0;
-                write(pipeJ0_P[WRITE], &vectorMensajesJx_P[jugadorActual], 1);
-            }
-            printf("El jugador %d ha terminado su turno\n",jugadorActual+1);
-            printf("La cola de turnos: ");
-            printqueue(colaTurnos);
-        }
-        else if (idJugador == 1) {
-            read(pipeJ1_P[READ], &mensajeJ1_P, 1);
-            if (mensajeJ1_P == 1) {
-                *ptrFlag = ejecutarTurno(jugadorActual, ptrTablero, ptrPosiciones, ptrSentido, colaTurnos);
-            }
-            else {
-                vectorMensajesJx_P[jugadorActual] = 0;
-                write(pipeJ1_P[WRITE], &vectorMensajesJx_P[jugadorActual], 1);
-            }
-            printf("El jugador %d ha terminado su turno\n",jugadorActual+1);
-            printf("La cola de turnos: ");
-            printqueue(colaTurnos);
-        }
-        else if (idJugador == 2) {
-            read(pipeJ2_P[READ], &mensajeJ2_P, 1);
-            if (mensajeJ0_P == 1) {
-                *ptrFlag = ejecutarTurno(jugadorActual, ptrTablero, ptrPosiciones, ptrSentido, colaTurnos);
-            }
-            else {
-                vectorMensajesJx_P[jugadorActual] = 0;
-                write(pipeJ2_P[WRITE], &vectorMensajesJx_P[jugadorActual], 1);
-            }
-            printf("El jugador %d ha terminado su turno\n",jugadorActual+1);
-            printf("La cola de turnos: ");
-            printqueue(colaTurnos);
-        }
-        else if (idJugador == 3) {
-            read(pipeJ3_P[READ], &mensajeJ3_P, 1);
-            if (mensajeJ3_P == 1) {
-                *ptrFlag = ejecutarTurno(jugadorActual, ptrTablero, ptrPosiciones, ptrSentido, colaTurnos);
-            }
-            else {
-                vectorMensajesJx_P[jugadorActual] = 0;
-                write(pipeJ3_P[WRITE], &vectorMensajesJx_P[jugadorActual], 1);
-            }
-            printf("El jugador %d ha terminado su turno\n",jugadorActual+1);
-            printf("La cola de turnos: ");
-            printqueue(colaTurnos);
-        }
-    }
-    cerrarPipes(idJugador);
-    if (idJugador == 0 || idJugador == 1 || idJugador == 2 || idJugador == 3)
+
+
+
+    /*cerrarPipes(idProceso);
+    if (idProceso == 0 || idProceso == 1 || idProceso == 2 || idProceso == 3)
         exit(1);
     jugador ganador;
     for (int i = 0; i < CANTIDADJUGADORES; i++){
@@ -316,7 +146,7 @@ int main() {
             }
         }
     }
-    printf("El juego ha terminado, el ganador es el jugador numero %d", ganador);
+    printf("El juego ha terminado, el ganador es el jugador numero %d\n", ganador);*/
     /*// BEGIN ejemplo*
     
     queue *colaTurnos = newqueue(4);
